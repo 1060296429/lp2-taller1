405eb4aa2375d74248a3bc8558dac3998fe98dcd
Dayana gomez <*dfnarin@siti.com*>
2026-09-16 20:04:58 -0500

    ajuste proyecto viejo

--- run.py
+++ run.py
@@ -10,7 +10,7 @@ from app import create_app
 app = create_app()
 
 if __name__ == "__main__":
-    # TODO: ejecutar la aplicación en modo debug=True mientras
-    # desarrollas (recarga automática + mensajes de error detallados).
-    # Pista: app.run(debug=True)
-    pass
+    # Ejecutar la aplicación en modo debug mientras desarrollas
+    app.run(debug=True)
+
+
